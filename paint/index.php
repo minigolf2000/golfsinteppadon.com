b847e99b7b76710881c9f165ec8868530571abc0
Golf Sinteppadon <golf.sinteppadon@gmail.com>
2012-01-18 00:12:43 -0800

    Paint now uses paintable.js

--- paint/index.php
+++ paint/index.php
@@ -2,17 +2,15 @@
 # Written by Golf Sinteppadon
 $title = "GS Paint";
 $includes = array('<link rel="stylesheet" type="text/css" href="paint.css" />',
-				  '<script src="/global/prototype.js" type="text/javascript"></script>',
-				  '<script src="paint.js" type="text/javascript"></script>');
+				  '<script src="http://code.jquery.com/jquery-latest.min.js"></script>',
+                  '<script src="paintable.js"></script>',
+				  '<script src="paint.js"></script>');
 include("../global/header.php");
 ?>
 		<div class="cloud">
 			<h2>GS Paint</h2>
 			<div id="frame">
-				<div id="toolbox">
-					<div id="tools"></div>
-					<div id="colors"></div>
-				</div>
+				<div id="toolbar"></div>
 				<canvas id="canvas" width="600" height="500">
 					<div id="nocanvas">
 						<h3><strong>Sorry!</strong></h3>
@@ -24,6 +22,7 @@ include("../global/header.php");
 					</div>
 				</canvas>
 			</div>
+            <p id="github" style="font-size: 1.3em;"><a href="https://github.com/MiniGolf2000/paintable"><img src="github.png" alt="Github" /><em>For the nerds:</em><span> Check out my </span><span id="paintable">.paintable()</span><span> jQuery plugin</span></a></p>
 		</div>
 <?php
 include("../global/footer.html");
