88ceade416234da122fc09b2cb90713bd67a8f95
Golf Sinteppadon <golf.sinteppadon@gmail.com>
2012-03-25 06:12:18 -0700

    Remove main.js from blocky

--- blocky/blocky.js
+++ blocky/blocky.js
@@ -1,6 +1,28 @@
-var blocky = (function () {
+(function () {
 
-    var self = {};
+    var keys = {
+        up: false,
+        left: false,
+        down: false,
+        right: false,
+        b: false,
+        a: false
+    };
+
+    var constants = {
+        WIDTH: 800,
+        HEIGHT: 552,
+        FPS: 50,
+        MAX_X_WALKING_VELOCITY: 5,
+        MAX_X_RUNNING_VELOCITY: 9,
+        RUNNING_ACCELERATION: .7,
+        WALKING_ACCELERATION: .7,
+        GRAVITY: 1,
+        FRICTION: .3,
+        JUMPING_FORCE: 7,
+        MIN_JUMP_FRAMES: 3, // Minimum number of frames that a jump will increase y velocity for
+        MAX_JUMP_FRAMES: 5 // Maximum number of frames that y velocity can be increased for
+    };
 
     var _ = {
         xPos: constants.WIDTH / 2,
@@ -20,15 +42,27 @@ var blocky = (function () {
         spriteY: 0
     };
 
-    self.initialize = function () {
+    _.initialize = function () {
+        $("#frame").height(constants.HEIGHT);
+        $(document).on('keyup keydown', _.onKeyEvent);
         $("#frame").append("<div id='blocky'></div>");
         _.$container = $("#blocky");
 
-        setInterval(function() {
+        setInterval(function () {
             _.update();
             _.render();
         }, 1000 / constants.FPS);
     };
+
+    /**
+     * Set the key matching e.keyCode in the keys field to be true if e.type == 'keydown'
+     * or false if e.type != 'keydown'
+     *
+     * @param e the key event
+     */
+    _.onKeyEvent = function (e) {
+        keys[({65: 'left', 83: 'down', 68: 'right', 75: 'b', 76: 'a'})[e.keyCode]] = (e.type == 'keydown');
+    }
 
     _.update = function () {
         // Jump initiate
@@ -144,6 +178,8 @@ var blocky = (function () {
         _.$container.css('backgroundPosition', -(_.spriteX * 128) + "px " + -(_.spriteY * 128) + "px");
     };
 
-    return self;
+    $(function () {
+        _.initialize();
+    });
 
 })();
